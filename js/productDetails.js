cf394e0fd57672b1e3832a7eeeaa98b92fe271c2
LillyTBui <>
2021-05-28 21:42:09 +0200

    product update

--- js/productDetails.js
+++ js/productDetails.js
@@ -5,7 +5,7 @@ const productImageContainer = document.querySelector(".product__img2");
 const breadcrumb = document.querySelector(".breadcrumb");
 const button = document.querySelector(".cart-btn");
 const cartList = document.querySelector(".cart-wrapper");
-let totalAddedItems = 0;
+let indexNumber;
 
 function createDetails(product){
     productDetailsContainer.innerHTML +=
@@ -39,7 +39,7 @@ addButton(item);
 const buttonToAdd = document.querySelector(".cta-cart");
 
 buttonToAdd.onclick = function(event){
-    totalAddedItems ++;
+
     showCart(item);
     const removeButton = document.querySelectorAll(".fa-times");
     removeButton.forEach(function(button){
@@ -49,37 +49,32 @@ buttonToAdd.onclick = function(event){
     })
     setTimeout(timer, 9000);
     cartList.style.display = "block";
+    
+    let oldIndex = JSON.parse(localStorage.getItem("index"));
+    oldIndex++;
+    const number = oldIndex.toString();
+    item["index"] = number;
+    localStorage.setItem("index", number);
+
     const oldCart = JSON.parse(localStorage.getItem("cartList"));
-    /*let numbOfProduct = JSON.parse(localStorage.getItem("totProducts"));
-    console.log(numberOfProduct);
-    numbOfProduct++;
-    console.log(numbOfProduct);*/
-    console.log(oldCart);
 
     if(oldCart === null ){
-        //let numberOfProduct = 1;
         const newCart = [item];
         localStorage.setItem("cartList", JSON.stringify(newCart));
-        //localStorage.setItem("totProducts", JSON.stringify(numberOfProduct));
     }
     else{
         oldCart.push(item);
         localStorage.setItem("cartList", JSON.stringify(oldCart));
-        //localStorage.setItem("totProduct", JSON.stringify(numbOfProduct));
     }
 }
 
-//localStorage.removeItem("cartList");
-//localStorage.removeItem("totProduct");
-
 function showCart(cartItems){
-    let totalPrice = cartItems.price*totalAddedItems;
     cartList.innerHTML = `<div class="cart">
         <div style="background-image: url(${cartItems.image})" class="cart-image"></div>
         <div class="cart-description">
             <h4>Added to cart</h4>
-            <h5 class="cart-description_items"><b>${totalAddedItems}x</b> ${cartItems.name}</h5>
-            <h3><b>${totalPrice},-</b></h3>
+            <h5 class="cart-description_items">${cartItems.name}</h5>
+            <h3><b>${cartItems.price},-</b></h3>
         </div>
 
         <i class="fas fa-times"></i>
--- js/shoppingbag.js
+++ js/shoppingbag.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const product = JSON.parse(localStorage.getItem("cartList"));
 console.log(product);
 
@@ -7,12 +9,11 @@ const totalProducts = document.querySelector(".total-products");
 let totalCost = 0;
 let totProducts = 0;
 
-if(product === null){
-    productWrapper.innerHTML = `<h3 class="empty-bag">There are no products in your shopping bag</h3>`;
-}
-else{
-    product.forEach(function(product){
-        productWrapper.innerHTML += `
+if (product === null) {
+  productWrapper.innerHTML = `<h3 class="empty-bag">There are no products in your shopping bag</h3>`;
+} else {
+  product.forEach(function (product) {
+    productWrapper.innerHTML += `
         <div class="product-added">
             <div style="background-image: url(${product.image})" class="shopping-cart__img"></div>
             <div class="product-added__section"> 
@@ -33,12 +34,32 @@ else{
                     <option value="3">3</option>
                   </select>
                   <p>${product.price},-</p>
-                  <i class="fas fa-trash-alt"></i>
+                  <i class="fas fa-trash-alt" data-product="${product.index}"></i>
         </div>`;
-        totalCost += product.price;
-        totProducts++;
-    })
+    totalCost += product.price;
+    totProducts++;
+  });
 }
+
+if(totProducts === 0){
+  productWrapper.innerHTML = `<h3 class="empty-bag">There are no products in your shopping bag</h3>`;
+  localStorage.removeItem("cartList");
+  localStorage.removeItem("index");
+}
+
+const removeItem = document.querySelectorAll(".fa-trash-alt");
+console.log(removeItem);
+removeItem.forEach(function(icon) {
+    icon.onclick = function(event) {
+      const oldList = JSON.parse(localStorage.getItem("cartList"));
+      const indexNumber = parseInt(event.target.dataset.product);
+      const itemToRemove = oldList.findIndex(item => parseInt(item.index) === indexNumber);
+      oldList.splice(itemToRemove, 1);
+      console.log(oldList);
+      localStorage.setItem("cartList", JSON.stringify(oldList));
+      window.location.reload();
+    };
+})
 
 totalProducts.innerHTML = `<p>Total products: ${totProducts}</p>`;
 
@@ -59,3 +80,4 @@ summary.innerHTML = `
         </div>
     </div>
     <a href="checkout-payment.html" class="cta summary-btn">Go to Payment</a>`;
+
